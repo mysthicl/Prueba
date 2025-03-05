9647a51add577b9bc99d6fc612b2091485258e21
mysthicl <irvingmachado08@gmail.com>
2025-03-05 12:02:50 -0600

    Subiendo modificaciones de practica 4

--- Practica 4/index.ts
+++ Practica 4/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, world! 2');
+enum nivelAcademico{
+    Primaria = "Primaria",
+    Secundaria = "Secundaria",
+    Universidad = "Universidad"
+}
+
+type alumno = {
+    nombre: string,
+    edad: number,
+    nivelAcademico: nivelAcademico
+}
+
+let alumno1: alumno = {nombre: "Juan", edad: 15, nivelAcademico: nivelAcademico.Primaria}
+let alumno2: alumno = {nombre: "Maria", edad: 20, nivelAcademico: nivelAcademico.Universidad} 
+console.log(alumno1);
+console.log(alumno2);
